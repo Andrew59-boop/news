746d3446d1747420df5263041ceb20c40ad5c81b
Andrew59-boop <millerfaabi@gmail.com>
2020-05-08 01:56:54 +0300

    eroku

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options,Config
+
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+
+    app.config.from_object(config_options[config_name])
+    app.config.from_object(Config)
+    
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting configuration
+    from .request import configure_request
+    configure_request(app)
+    
+
+
+
+    return app
