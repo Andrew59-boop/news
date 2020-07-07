8188b1dbefd4eb8be4b02794c0013643756183fb
Andrew59-boop <millerfaabi@gmail.com>
2020-07-07 11:02:43 +0300

    serving articles

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config:
     General configuration parent class
     '''
    
-    SOURCE_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey=b50dd86155244655ab3cb391841bd78a'
-    ARTICLE_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    SOURCE_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey=b4bd6265ea14495ea0bac2beda504a2e'
+    ARTICLE_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey=b4bd6265ea14495ea0bac2beda504a2e'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
